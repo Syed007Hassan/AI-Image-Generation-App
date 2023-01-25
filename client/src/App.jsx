173a775e449bcc01d9cf03675b2e62f4370a5a4e
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-01-25 23:16:45 +0500

    initial

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { Home, CreatePost } from "./pages";
+
+const App = () => {
+  return (
+    <Router>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        {" "}
+      </header>
+    </Router>
+  );
+};
+
+export default App;
