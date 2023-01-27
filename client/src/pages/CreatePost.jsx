4c70c149f72f1287272a1bf6a9f4ea57e47c2739
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-01-28 02:51:55 +0500

    api added

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,8 +18,56 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch("http://localhost:5000/api/v1/dalle", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
+
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (error) {
+        alert(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert("Please enter a prompt");
+    }
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/v1/posts", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        });
+
+        await response.json();
+        alert("Success");
+        navigate("/");
+      } catch (error) {
+        alert(error);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert("Please enter a prompt and generate an image");
+    }
   };
 
   const handleChange = (e) => {
@@ -31,8 +79,6 @@ const CreatePost = () => {
     setForm({ ...form, prompt: randomPrompt });
   };
 
-  const generateImage = () => {};
-
   return (
     <section>
       <div>
@@ -43,13 +89,13 @@ const CreatePost = () => {
         </p>
       </div>
 
-      <form className="mt-16 max-w-3x1" onSubmit={handleSubmit}>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField
-            labelName="Name"
+            labelName="Your Name"
             type="text"
             name="name"
-            placeholder="Jon Doe"
+            placeholder="Ex., john doe"
             value={form.name}
             handleChange={handleChange}
           />
@@ -80,7 +126,7 @@ const CreatePost = () => {
             )}
 
             {generatingImg && (
-              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0,0.5)] rounded-lg ">
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
                 <Loader />
               </div>
             )}
@@ -99,15 +145,14 @@ const CreatePost = () => {
 
         <div className="mt-10">
           <p className="mt-2 text-[#666e75] text-[14px]">
-            {" "}
-            Once you have created the image you want, you can share it others in
-            the community.{" "}
+            ** Once you have created the image you want, you can share it with
+            others in the community **
           </p>
           <button
-            type="button"
+            type="submit"
             className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
-            {loading ? "Sharing.." : "Share with community"}
+            {loading ? "Sharing..." : "Share with the Community"}
           </button>
         </div>
       </form>
--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDb from "./connect.js";
+import bodyParser from "body-parser";
+
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +13,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//middleware
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 let PORT = process.env.PORT || 5000;
 
