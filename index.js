372b1cc8be56d2f8e6ffe9e1c1c42cc11a689367
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-03-20 09:49:09 +0500

    updated

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
